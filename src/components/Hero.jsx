3487ed005601ea8c310f166a2a3fd29312c60f0d
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2023-04-30 00:32:27 +0600

    canvas failed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
-import { styles } from "../styles";
-import { motion } from "framer-motion";
-import { ComputersCanvas } from "./canvas";
-
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -10,11 +11,11 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
+        {/* // eslint-disable-next-line react/react-in-jsx-scope */}
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Abu Saleh Faysal</span>
@@ -24,9 +25,9 @@ const Hero = () => {
             interfaces and web applications{" "}
           </p>
         </div>
-
-        <ComputersCanvas />
       </div>
+
+      <ComputersCanvas />
     </section>
   );
 };
